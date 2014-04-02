bb7e1d64f24f7e7c5c5eb8fe4aa4172999643c2d
ganondalf <cganon@gmail.com>
2014-04-02 11:33:59 -0400

    fixed git api error

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,7 @@ class IssuesController < ApplicationController
 
   def index
     @git_name = current_user.github_handle
-    @response = HTTParty.get('https://api.github.com/users/'@git_name'/gists',
+    @response = HTTParty.get("https://api.github.com/users/#{@git_name}/gists",
               :headers => {"Authorization" => "token #{session[:api_token]}",
                           "User-Agent" => "ganondalf-gather"})
 
